fdce296c0e11682a6384fb6636b3987f41d3135e
priscillamc <priscillamc@users.noreply.github.com>
2017-03-20 15:16:10 -0400

    Adds IProduct data type, custom styles for component, lifecycle hook

--- APM/app/products/product-list.component.ts
+++ APM/app/products/product-list.component.ts
@@ -1,16 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { IProduct } from './product';
 
 @Component({
     selector: 'pm-products',
-    templateUrl: 'app/products/product-list.component.html'
+    templateUrl: 'app/products/product-list.component.html',
+	styleUrls: ['app/products/product-list.component.css']
 })
-export class ProductListComponent {
+export class ProductListComponent implements OnInit {
     pageTitle: string = 'Product List';
     imageWidth: number = 50;
 	imageMargin: number = 2;
 	showImage: boolean = false;
 	listFilter: string = 'cart';
-    products: any[] = [
+    products: IProduct[] = [
 		{
 			"productId": 1,
 			"productName": "Leaf Rake",
@@ -63,7 +66,11 @@ export class ProductListComponent {
 		}
     ];
 	
-toggleImage(): void {
-	this.showImage = !this.showImage;
-}
+	toggleImage(): void {
+		this.showImage = !this.showImage;
+	}
+	
+	ngOnInit(): void {
+		console.log('In OnInit');
+	}
 }
